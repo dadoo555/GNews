0398cbeb71976aaf991b10dddfe4b6ee3be9f7a2
Eduardo Grigull <dado.gri@gmail.com>
2023-08-23 21:05:34 +0200

    major changes

--- db/configureSQL.sql
+++ db/configureSQL.sql
@@ -6,14 +6,14 @@ CREATE TABLE `authors` (
   `last_name` varchar(45) DEFAULT NULL,
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `index_homepage` (
   `idx` int NOT NULL AUTO_INCREMENT,
   `index_id` varchar(10) NOT NULL,
   `news_id` varchar(10) NOT NULL,
   PRIMARY KEY (`idx`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `news` (
   `news_id` int NOT NULL AUTO_INCREMENT,
@@ -27,7 +27,7 @@ CREATE TABLE `news` (
   `locality` varchar(45) NOT NULL,
   `author_id` varchar(45) NOT NULL,
   PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `pictures` (
   `picture_id` int NOT NULL AUTO_INCREMENT,
@@ -35,4 +35,4 @@ CREATE TABLE `pictures` (
   `path` varchar(300) NOT NULL,
   `news_id` int NOT NULL,
   PRIMARY KEY (`picture_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
